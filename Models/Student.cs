0dfb348ea9c619ab74abf266a0ac574efc6a7974
Gyuris Csaba <gycsaba72@gmail.com>
2024-02-20 12:07:03 +0100

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace StudentProject.Models
+{
+    public enum SchoolClassType { ClassA, ClassB, ClassC}
+
+    public class Student
+    {
+        public Student()
+        {
+            Id = Guid.Empty;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            BirthDay = new DateTime();
+            SchoolYear = 9;
+            SchoolClass = SchoolClassType.ClassA;
+            IsWoomen = false;
+            EducationLevel = string.Empty;
+        }
+
+        public Student(Guid id, string firstName, string lastName, DateTime birthDay, int schoolYear, SchoolClassType schoolClass, bool isWoomen, string educationLevel)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDay = birthDay;
+            SchoolYear = schoolYear;
+            SchoolClass = schoolClass;
+            IsWoomen = isWoomen;
+            EducationLevel = educationLevel;
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SchoolYear { get; set; }
+        public DateTime BirthDay { get; set; }
+        public SchoolClassType SchoolClass { get; set; }
+        public string EducationLevel { get; set; }
+        public bool IsWoomen { get; set; }
+        public bool HasId => Id != Guid.Empty;
+        public string HungarianFullName
+        {
+            set { }
+            get => $"{LastName} {FirstName}";
+        }
+
+        public void Set(Student student)
+        {
+            Id= student.Id;
+            FirstName= student.FirstName;
+            LastName= student.LastName;
+            BirthDay = student.BirthDay;
+            SchoolClass= student.SchoolClass;
+            EducationLevel= student.EducationLevel;
+            IsWoomen= student.IsWoomen;
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} ({SchoolYear}.{SchoolClass}) {BirthDay.ToString("d", new CultureInfo("hu-HU"))} {EducationLevel}";
+        }
+    }
+}
---
+++ Repos/StudentRepo.cs
@@ -0,0 +1,97 @@
+﻿using StudentProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kreta.Desktop.Repos
+{
+    public class StudentRepo
+    {
+        #region Database
+        private List<Student> _students = new()
+        {
+            new() {
+                Id=Guid.NewGuid(),
+                FirstName="János",
+                LastName="Jegy",
+                IsWoomen=false,
+                BirthDay=new DateTime(2021,3,13),
+                SchoolYear=9,
+                SchoolClass = SchoolClassType.ClassA,
+                EducationLevel="szakképzés"
+            },
+            new() {
+                Id=Guid.NewGuid(),
+                FirstName="Szonja",
+                LastName="Stréber",
+                BirthDay=new DateTime(2020,4,24),
+                IsWoomen=true,
+                SchoolYear=10,
+                SchoolClass = SchoolClassType.ClassB,
+            },
+            new() {
+                Id=Guid.NewGuid(),
+                FirstName="Ubul",
+                LastName="Ugráló",
+                IsWoomen=false,
+                BirthDay=new DateTime(2020,2,11),
+                SchoolYear=10,
+                SchoolClass = SchoolClassType.ClassB,
+                EducationLevel="érettségi"
+            },
+            new() {
+                Id=Guid.NewGuid(),
+                FirstName="Kati",
+                LastName="Késő",
+                IsWoomen=true,
+                BirthDay=new DateTime(2019,2,11),
+                SchoolYear=12,
+                SchoolClass = SchoolClassType.ClassA,
+                EducationLevel="érettségi"
+            },
+            new() {
+                Id=Guid.NewGuid(),
+                FirstName="Kenéz",
+                LastName="Kísérletező",
+                IsWoomen=false,
+                BirthDay=new DateTime(2017,2,11),
+                SchoolYear=14,
+                SchoolClass = SchoolClassType.ClassA,
+                EducationLevel="szakképzés"
+            }
+        };
+        #endregion
+
+        public List<Student> FindAll()
+        {
+            return _students;
+        }
+        public void Insert(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public void Update(Student student)
+        {
+            if (!student.HasId)
+            {
+                Insert(student);
+            }
+            else
+            {
+                Student? studentToUpdate = _students.FirstOrDefault(s => s.Id == student.Id);
+                if (studentToUpdate is not null)
+                {
+                    studentToUpdate.Set(student);
+                }
+            }
+        }
+
+        public void Delete(Student student)
+        {
+            _students.Remove(student);
+        }
+
+
+    }
+}
